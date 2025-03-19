7bd88374a3048c8fb4a0a1f8888e966129903307
wolfie-killua <112668928+wolfie-killua@users.noreply.github.com>
2025-03-20 06:57:53 +0800

    Updates

---
+++ static/js/952.59dbbd3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkevent_api=self.webpackChunkevent_api||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.59dbbd3b.chunk.js.map
